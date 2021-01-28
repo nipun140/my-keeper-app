d8d692677b30cd19423e1b413b5493c984053ba5
nipun <nipungarg.nipun@gmail.com>
2021-01-28 13:21:51 +0530

    Updates

---
+++ precache-manifest.a0e6e1612b5fce0d3f76370b444b3456.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86b55b206d83bddcc636dc342e6c310",
+    "url": "/my-keeper-app/index.html"
+  },
+  {
+    "revision": "d17d7a910a50745f733f",
+    "url": "/my-keeper-app/static/js/2.602816e2.chunk.js"
+  },
+  {
+    "revision": "721539fc7825c126f6ca",
+    "url": "/my-keeper-app/static/js/main.e59f2689.chunk.js"
+  },
+  {
+    "revision": "f5416f7e7d3f28517049",
+    "url": "/my-keeper-app/static/js/runtime-main.bd17a178.js"
+  }
+]);
